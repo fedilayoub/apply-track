0fc547b42e183346fdb88e107801df03d6d425cf
fedilayoub <ayoubzahi2016@gmail.com>
2024-10-19 14:22:36 +0100

    first commit

---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+import Wrapper from '../assets/wrappers/LandingPage';
+import main from '../assets/images/main.svg';
+import { Link } from 'react-router-dom';
+import { Logo } from '../components';
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            applyTrack is a job application tracking platform that helps you
+            keep track of your job applications by storing and displaying
+            information about the jobs you've applied to, including the job
+            title, company, position, job location, job type, job status, and
+            creation date.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn ">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
