ef9c04de1857d7f583642d8a97d80bb2bde3a9d8
fedilayoub <ayoubzahi2016@gmail.com>
2024-10-20 13:50:40 +0100

    changed charts color

--- client/src/components/AreaChart.jsx
+++ client/src/components/AreaChart.jsx
@@ -16,7 +16,7 @@ const AreaChartComponent = ({ data }) => {
         <XAxis dataKey='date' />
         <YAxis allowDecimals={false} />
         <Tooltip />
-        <Area type='monotone' dataKey='count' stroke='#2cb1bc' fill='#bef8fd' />
+        <Area type='monotone' dataKey='count' stroke='#2196f3' fill='#5faff0' />
       </AreaChart>
     </ResponsiveContainer>
   );
